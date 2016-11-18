847d8c022ccb9fd87c685caf68b4b90c2aef9628
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-19 00:54:37 +0900

    Update init writing for c++11

--- cirkit_unit03_driver/src/cirkit_unit03_driver.cpp
+++ cirkit_unit03_driver/src/cirkit_unit03_driver.cpp
@@ -17,24 +17,24 @@
 using namespace std; // FIXME: this software is library to cirkit_unit03_driver_node, don't erosion grobal area.
 
 cirkit::CirkitUnit03Driver::CirkitUnit03Driver(const std::string& imcs01_port_, const ros::NodeHandle& nh)
-: nh_(nh),
-  rate_(100),
-  odom_pub_(nh_.advertise<nav_msgs::Odometry>("/odom", 1)),
-  steer_pub_(nh_.advertise<geometry_msgs::Twist>("/steer_ctrl", 1)),
-  cmd_vel_sub_(nh_.subscribe<geometry_msgs::Twist>("/cmd_vel", 1, boost::bind(&cirkit::CirkitUnit03Driver::cmdVelReceived, this, _1))),
-  odom_broadcaster_(),
-  imcs01_port_(), // over write by rosparam
-  current_time_(),
-  last_time_(),
-  access_mutex_(),
-  steer_dir_(),
-  cirkit_unit03_(new cirkit::ThirdRobotInterface(imcs01_port_, 0))
+: nh_ {nh},
+  rate_ {100},
+  odom_pub_ {nh_.advertise<nav_msgs::Odometry>("/odom", 1)},
+  steer_pub_ {nh_.advertise<geometry_msgs::Twist>("/steer_ctrl", 1)},
+  cmd_vel_sub_ {nh_.subscribe<geometry_msgs::Twist>("/cmd_vel", 1, boost::bind(&cirkit::CirkitUnit03Driver::cmdVelReceived, this, _1))},
+  odom_broadcaster_ {},
+  imcs01_port_ {}, // over write by rosparam
+  current_time_ {},
+  last_time_ {},
+  access_mutex_ {},
+  steer_dir_ {},
+  cirkit_unit03_ {new cirkit::ThirdRobotInterface(imcs01_port_, 0)}
 {
-  double pulse_rate = 40.0;
-  double geer_rate = 33.0;
-  double wheel_diameter_right = 0.275;
-  double wheel_diameter_left = 0.275;
-  double tred_width = 0.595;
+  double pulse_rate {40.0};
+  double geer_rate {33.0};
+  double wheel_diameter_right {0.275};
+  double wheel_diameter_left {0.275};
+  double tred_width {0.595};
   n.param("pulse_rate", pulse_rate, pulse_rate);
   n.param("geer_rate", geer_rate, geer_rate);
   n.param("wheel_diameter_right", wheel_diameter_right, wheel_diameter_right);
@@ -52,13 +52,13 @@ cirkit::CirkitUnit03Driver::~CirkitUnit03Driver() {
 }
 
 void cirkit::CirkitUnit03Driver::resetCommunication() {
-  if(cirkit_unit03_->openSerialPort() == 0) {
+  if (cirkit_unit03_->openSerialPort() == 0) {
 	  ROS_INFO("Connected to cirkit unit03.");
 	  cirkit_unit03_->driveDirect(0, 0);
 	} else {
 	  ROS_FATAL("Could not connect to cirkit unit03.");
     delete cirkit_unit03_;
-    throw runtime_error("Could not connect to cirkit unit03");
+    throw runtime_error {"Could not connect to cirkit unit03"};
 	}
 
   cirkit_unit03_->resetOdometry();
@@ -70,14 +70,14 @@ void cirkit::CirkitUnit03Driver::run() {
   double vel_x, vel_y, vel_yaw;
   double dt;
 
-  while(nh_.ok()) {
+  while (nh_.ok()) {
     current_time_ = ros::Time::now();
     last_x = cirkit_unit03_->odometry_x_;
     last_y = cirkit_unit03_->odometry_y_;
     last_yaw = cirkit_unit03_->odometry_yaw_;
     {
-      boost::mutex::scoped_lock(access_mutex_); // why use mutex?
-      if( cirkit_unit03_->getEncoderPacket() == -1) ROS_ERROR("Could not retrieve encoder packet.");
+      boost::mutex::scoped_lock {access_mutex_}; // why use mutex?
+      if (cirkit_unit03_->getEncoderPacket() == -1) ROS_ERROR("Could not retrieve encoder packet.");
       else cirkit_unit03_->calculateOdometry();
     }
     dt = (current_time_ - last_time_).toSec();
@@ -85,7 +85,7 @@ void cirkit::CirkitUnit03Driver::run() {
     vel_y = (cirkit_unit03_->odometry_y_ - last_y)/dt;
     vel_yaw = (cirkit_unit03_->odometry_yaw_ - last_yaw)/dt;
 
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(cirkit_unit03_->odometry_yaw_);
+    geometry_msgs::Quaternion odom_quat {tf::createQuaternionMsgFromYaw(cirkit_unit03_->odometry_yaw_)};
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time_;
     odom_trans.header.frame_id = "odom";
@@ -123,9 +123,9 @@ void cirkit::CirkitUnit03Driver::run() {
 }
 
 void cirkit::CirkitUnit03Driver::cmdVelReceived(const geometry_msgs::Twist::ConstPtr& cmd_vel) {
-  static int steer = 0;
+  static int steer {0};
   {
-    boost::mutex::scoped_lock(access_mutex_); // why use mutex?
+    boost::mutex::scoped_lock {access_mutex_}; // why use mutex?
     steer_dir_ = cirkit_unit03_->drive(cmd_vel->linear.x, cmd_vel->angular.z);
   }
   steer_pub_.publish(steer_dir_);

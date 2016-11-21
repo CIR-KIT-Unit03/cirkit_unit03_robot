73a11b69c53ddae9785e69c8027b53600d3df094
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-21 16:47:50 +0900

    Update wrting for ThirdRobotInterface

--- cirkit_unit03_driver/include/ThirdRobotInterface/ThirdRobotInterface.h
+++ cirkit_unit03_driver/include/ThirdRobotInterface/ThirdRobotInterface.h
@@ -16,12 +16,14 @@
 // For old c
 #include <termios.h>
 
-enum {
+enum
+{
   FRONT,
   REAR
 };
 
-enum {
+enum
+{
   ROBOT_STASIS_FORWARD,
   ROBOT_STASIS_FORWARD_STOP,
   ROBOT_STASIS_BACK,
@@ -29,7 +31,8 @@ enum {
   ROBOT_STASIS_OTHERWISE
 };
 
-enum {
+enum
+{
   FORWARD_MODE,
   FORWARD_STOP_MODE,
   BACK_MODE,
@@ -39,25 +42,30 @@ enum {
 
 
 template<typename N, typename M>
-inline double MIN(const N& a, const M& b) {
+inline double MIN(const N& a, const M& b)
+{
   return a < b ? a : b;
 }
 
 template<typename N, typename M>
-inline double MAX(const N& a, const M& b) {
+inline double MAX(const N& a, const M& b)
+{
   return a > b ? a : b;
 }
 
 template<typename T>
-inline double NORMALIZE(const T& z) {
+inline double NORMALIZE(const T& z)
+{
   return atan2(sin(z), cos(z));
 }
 
 int plus_or_minus(double value);
 
-namespace cirkit {
+namespace cirkit
+{
 
-class ThirdRobotInterface {
+class ThirdRobotInterface
+{
 public:
   //! Constructor
   ThirdRobotInterface(const std::string& new_serial_port_imcs01, int new_baudrate_imcs01);
@@ -125,18 +133,18 @@ protected:
   termios newtio_imcs01;
   //! Delta rear encoder counts.
   //! 0 is right, 1 is left.
-  int delta_rear_encoder_counts[2] = {0, 0};
+  int delta_rear_encoder_counts[2] {0, 0};
   //! Last rear encoder counts reading. For odometry calculation.
   //! 0 is right, 1 is left.
-  int last_rear_encoder_counts[2] = {0, 0};
+  int last_rear_encoder_counts[2] {0, 0};
   //! Last time reading encoder
   double last_rear_encoder_time;
   //! Delta time
   double delta_rear_encoder_time;
   //! Delta dist
   //! 0 is right, 1 is left.
-  double delta_dist[2] = {0, 0};
-  double last_delta_dist[2] = {0, 0};
+  double delta_dist[2] {0, 0};
+  double last_delta_dist[2] {0, 0};
   //! [k-1]のyaw
   double last_odometry_yaw = 0.0;
   //! num of pulse
@@ -144,7 +152,7 @@ protected:
   //! GEER_RATE
   double GeerRate = 33.0;
   //! Wheel Diameter[m]
-  double WheelDiameter[2] = {0.2705, 0.275};
+  double WheelDiameter[2] {0.2705, 0.275};
   //! Tred width[m]
   double TredWidth = 0.595;
   //! Linear velocity

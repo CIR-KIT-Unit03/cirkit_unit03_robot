d1523b6de6eea36805ecd9c6e6d6e560ee4f28cf
Arita Yuta <k104009y@mail.kyutech.jp>
2016-12-23 22:02:24 +0900

    add updating virtual joint state

--- cirkit_unit03_base/src/cirkit_unit03_hw.cpp
+++ cirkit_unit03_base/src/cirkit_unit03_hw.cpp
@@ -9,6 +9,13 @@
 
 #include "ixis_imcs01_driver/ixis_imcs01_driver.h"
 
+#define VIRTUAL_JOINT_IND_RIGHT_REAR 0
+#define VIRTUAL_JOINT_IND_LEFT_REAR 1
+#define VIRTUAL_JOINT_IND_RIGHT_FRONT 2
+#define VIRTUAL_JOINT_IND_LEFT_FRONT 3
+#define VIRTUAL_JOINT_IND_RIGHT_FRONT_STEER 4
+#define VIRTUAL_JOINT_IND_LEFT_FRONT_STEER 5
+
 class CirkitUnit03HardwareInterface
   : public hardware_interface::RobotHW
 {
@@ -20,8 +27,11 @@ public:
   void write();
 protected:
   void publishSteer(double angle_cmd);
+  void registerVirtualJointState(std::vector<double> virtual_wheels_pos_,
+                                 std::vector<double> virtual_wheels_vel_,
+                                 std::vector<double> virtual_wheels_eff_,
+                                 std::vector<string> virtual_wheels_names)
   ros::NodeHandle nh_;
-
   //hardware_interface::JointStateInterface front_steer_jnt_state_interface_;
   hardware_interface::PositionJointInterface front_steer_jnt_pos_cmd_interface_;
   double front_steer_pos_;
@@ -38,6 +48,10 @@ protected:
 
   hardware_interface::JointStateInterface joint_state_interface_;
 
+  std::vector<double> virtual_wheels_pos_;
+  std::vector<double> virtual_wheels_vel_;
+  std::vector<double> virtual_wheels_eff_;
+
   ros::Publisher steer_cmd_publisher_;
   IxisImcs01Driver ixis_imcs01_driver_;
 };
@@ -48,9 +62,20 @@ CirkitUnit03HardwareInterface::CirkitUnit03HardwareInterface(const std::string& 
     steer_cmd_publisher_(nh_.advertise<geometry_msgs::Twist>("/steer_ctrl", 1))
 {
   ros::NodeHandle n("~");
-  std::string front_steer_joint_name("front_steer_joint");
-  std::string rear_wheel_joint_name("rear_wheel_joint");
-
+  std::string front_steer_joint_name("front_steer_joint"); // for steer_drive_ros
+  std::string rear_wheel_joint_name("rear_wheel_joint"); // for steer_drive_ros
+  std::vector<std::string> virtual_wheels_names;
+  virtual_wheels_names.push_back("base_to_right_rear_wheel");
+  virtual_wheels_names.push_back("base_to_left_rear_wheel");
+  virtual_wheels_names.push_back("base_to_right_front_wheel");
+  virtual_wheels_names.push_back("base_to_left_front_wheel");
+  virtual_wheels_names.push_back("base_to_right_front_steer");
+  virtual_wheels_names.push_back("base_to_left_front_steer");
+  // front right, front left, rear right, rear left and steers = 6
+  virtual_wheels_vel_.resize(6);
+  virtual_wheels_pos_.resize(6);
+  virtual_wheels_eff_.resize(6);
+  
   hardware_interface::JointStateHandle front_steer_state_handle(front_steer_joint_name, &front_steer_pos_, &front_steer_vel_, &front_steer_eff_);
   joint_state_interface_.registerHandle(front_steer_state_handle);
   hardware_interface::JointHandle front_steer_pos_cmd_handle(joint_state_interface_.getHandle(front_steer_joint_name), &front_steer_pos_cmd_);
@@ -61,9 +86,23 @@ CirkitUnit03HardwareInterface::CirkitUnit03HardwareInterface(const std::string& 
   hardware_interface::JointHandle rear_wheel_vel_cmd_handle(joint_state_interface_.getHandle(rear_wheel_joint_name), &rear_wheel_vel_cmd_);
   rear_wheel_jnt_vel_cmd_interface_.registerHandle(rear_wheel_vel_cmd_handle);
 
+  this->registerVirtualJointState(virtual_wheels_pos_, virtual_wheels_vel_, virtual_wheels_eff_,
+                                  virtual_wheels_names);
+  
   registerInterface(&front_steer_jnt_pos_cmd_interface_);
   registerInterface(&rear_wheel_jnt_vel_cmd_interface_);
   registerInterface(&joint_state_interface_);
+}
+
+void CirkitUnit03HardwareInterface::registerVirtualJointState(std::vector<double> virtual_wheels_pos_,
+                                                              std::vector<double> virtual_wheels_vel_,
+                                                              std::vector<double> virtual_wheels_eff_,
+                                                              std::vector<string> virtual_wheels_names)
+{
+ for (int i = 0; i < 6; ++i) {
+   hardware_interface::JointStateHandle state_handle(virtual_wheels_names[i], &virtual_wheels_pos_[i], &virtual_wheels_vel_[i], &virtual_wheels_eff_[i]);
+   joint_state_interface_.registerHandle(state_handle);
+ }
 }
 
 void CirkitUnit03HardwareInterface::read()
@@ -75,6 +114,20 @@ void CirkitUnit03HardwareInterface::read()
                      + joints_state.position[JOINT_INDEX_REAR_LEFT]) / 2.0;
   rear_wheel_vel_ = (joints_state.velocity[JOINT_INDEX_REAR_RIGHT]
                      + joints_state.velocity[JOINT_INDEX_REAR_LEFT]) / 2.0;
+  const double h = 0.75;
+  const double w = 0.28;
+  virtual_wheels_vel_[VIRTUAL_JOINT_IND_RIGHT_REAR]  = joints_state.velocity[JOINT_INDEX_REAR_RIGHT];
+  virtual_wheels_pos_[VIRTUAL_JOINT_IND_RIGHT_REAR]  = joints_state.position[JOINT_INDEX_REAR_RIGHT];
+  virtual_wheels_vel_[VIRTUAL_JOINT_IND_LEFT_REAR]   = joints_state.velocity[JOINT_INDEX_REAR_LEFT];
+  virtual_wheels_pos_[VIRTUAL_JOINT_IND_LEFT_REAR]   = joints_state.position[JOINT_INDEX_REAR_LEFT];
+  virtual_wheels_vel_[VIRTUAL_JOINT_IND_RIGHT_FRONT] = virtual_wheels_vel_[VIRTUAL_JOINT_IND_RIGHT_REAR];
+  virtual_wheels_pos_[VIRTUAL_JOINT_IND_RIGHT_FRONT] = virtual_wheels_pos_[VIRTUAL_JOINT_IND_RIGHT_REAR];
+  virtual_wheels_vel_[VIRTUAL_JOINT_IND_LEFT_FRONT]  = virtual_wheels_vel_[VIRTUAL_JOINT_IND_LEFT_REAR];
+  virtual_wheels_pos_[VIRTUAL_JOINT_IND_LEFT_FRONT]  = virtual_wheels_pos_[VIRTUAL_JOINT_IND_LEFT_REAR];
+  virtual_wheels_pos_[VIRTUAL_JOINT_IND_RIGHT_FRONT_STEER] = atan2(2.0*h*tan(front_steer_pos_),
+                                                                   2*h + w/2.0*tan(front_steer_pos_));
+  virtual_wheels_pos_[VIRTUAL_JOINT_IND_LEFT_FRONT_STEER]  = atan2(2.0*h*tan(front_steer_pos_),
+                                                                   2*h - w/2.0*tan(front_steer_pos_));
 }
 
 void CirkitUnit03HardwareInterface::write()
